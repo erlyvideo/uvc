d91211f6b6800675709ee8ef12e0bd191441948e
Max Lapshin <max@maxidoors.ru>
2011-08-07 23:23:06 +0400

    Capture audio from stdout and send to streamer

--- src/uvc_publisher.erl
+++ src/uvc_publisher.erl
@@ -20,7 +20,10 @@
   rtmp,
   base_vpts,
   start,
-  stream
+  stream,
+  buffer = [],
+  audio_count = 0,
+  video_count = 0
 }).
 
 publish(URL, Options) ->
@@ -31,6 +34,7 @@ publish(URL, Options) ->
 
 init([URL, Options]) ->
   {ok, UVC} = uvc4erl:open(Options),
+  % UVC = undefined,
   
   {W,H} = proplists:get_value(size, Options),
   {ok, RTMP} = rtmp_socket:connect(URL),
@@ -49,7 +53,10 @@ init([URL, Options]) ->
   
   SampleRate = 32000,
   Channels = 2,
-  {ok, Capture} = audiocapture:start(-1, SampleRate, Channels),
+  % {ok, Capture} = audiocapture:start(SampleRate, Channels),
+  Capture = open_port({spawn, "arecord --disable-resample -c 2 -D default:CARD=U0x46d0x821 -r 32000 -f S16_LE"}, [stream, binary]),
+  put(pcm_buf, <<>>),
+  put(pcm_dts, 0),
   {ok, AACEnc, AAConfig} = ems_sound2:init_faac(<<SampleRate:32/little, Channels:32/little>>),
   
   send_frame(RTMP, Stream, #video_frame{
@@ -120,13 +127,31 @@ handle_call(Call, _From, State) ->
 
 handle_cast(Cast, State) ->
   {stop, {unknown_cast, Cast}, State}.
+  
+
+
+enqueue(#video_frame{} = Frame, #publisher{buffer = Buf1, rtmp = RTMP, stream = Stream} = State) ->
+  Buf2 = lists:keysort(#video_frame.dts, [Frame|Buf1]),
+  Buf3 = case Buf2 of
+    [F1|Frames] when length(Frames) >= 6 ->
+      io:format("~4s ~8B ~8B~n", [F1#video_frame.codec, F1#video_frame.dts, timer:now_diff(erlang:now(), State#publisher.start) div 1000]),
+      send_frame(RTMP, Stream, F1),
+      Frames;
+    _ ->
+      Buf2
+  end,
+  State#publisher{buffer = Buf3}.
 
 % handle_info({uvc4erl, UVC, Codec, PTS1, RAW}, #publisher{base_vpts = undefined} = State) ->
 %   handle_info({uvc4erl, UVC, Codec, 0, RAW}, State#publisher{base_vpts = PTS1});
+% 
+% handle_info({uvc4erl, UVC, Codec, PTS1, RAW}, State) ->
+%   {noreply, State};
 
 handle_info({uvc4erl, UVC, Codec, _PTS1, Jpeg}, #publisher{base_vpts = BaseVPTS, width = Width, height = Height, x264 = X264} = State) ->
   % PTS = PTS1 - BaseVPTS,
   Drop = drop(),
+  % Drop = 0,
 
   T1 = erlang:now(),
   PTS = timer:now_diff(T1, State#publisher.start) div 1000,
@@ -135,35 +160,45 @@ handle_info({uvc4erl, UVC, Codec, _PTS1, Jpeg}, #publisher{base_vpts = BaseVPTS,
     yuv -> Jpeg
   end,
   T2 = erlang:now(),
-  case ems_video:yuv_x264(X264, YUV, PTS) of
-    ok -> ok;
+  State1 = case ems_video:yuv_x264(X264, YUV, PTS) of
+    ok -> State;
     {ok, Flavor, PTSEnc, DTSEnc, H264} ->
       T3 = erlang:now(),
-      io:format("~4s ~5B ~px~p, (~p, ~p), ~p~n", [h264, DTSEnc, Width, Height, timer:now_diff(T2,T1) div 1000, timer:now_diff(T3,T2) div 1000, Drop]),
-    
-      send_frame(State#publisher.rtmp, State#publisher.stream, #video_frame{
+      enqueue(#video_frame{
         content = video,
         flavor = Flavor,
         codec = h264,
         pts = PTSEnc,
         dts = DTSEnc,
         body = H264
-      })
+      }, State)
   
     % file:write(get(file), H264)
   end,
   
-  {noreply, State};
+  VideoCount = State#publisher.video_count + 1,
+  % ?D({v,VideoCount, VideoCount*50, timer:now_diff(erlang:now(),State#publisher.start) div 1000, Drop}),
+  {noreply, State1#publisher{video_count = VideoCount}};
 
+handle_info({Capture, {data, Raw}}, State) ->
+  case <<(get(pcm_buf))/binary, Raw/binary>> of
+    <<Bin:4096/binary, Rest/binary>> ->
+      put(pcm_buf, Rest),
+      DTS = get(pcm_dts) div (32*4),
+      put(pcm_dts, get(pcm_dts)+size(Bin)),
+      handle_info({audiocapture, Capture, DTS, Bin}, State);
+    Bin ->
+      put(pcm_buf, Bin),
+      {noreply, State}
+  end;
 
 handle_info({audiocapture, Capture, DTS, PCM}, #publisher{faac = AACEnc} = State) ->
   T1 = erlang:now(),
-  case ems_sound2:pcm_aac(AACEnc, PCM) of
-    undefined -> ok;
+  State1 = case ems_sound2:pcm_aac(AACEnc, PCM) of
+    undefined -> State;
     AAC -> 
       T2 = erlang:now(),
-      io:format("~4s ~5B (~p)~n", [aac, DTS, timer:now_diff(T2,T1) div 1000]),
-      send_frame(State#publisher.rtmp, State#publisher.stream, #video_frame{
+      enqueue(#video_frame{
         content = audio,
         flavor = frame,
         codec = aac,
@@ -171,9 +206,14 @@ handle_info({audiocapture, Capture, DTS, PCM}, #publisher{faac = AACEnc} = State
         pts = DTS,
         dts = DTS,
         body = AAC
-      })
+      }, State)
   end,
-  {noreply, State};
+  
+  AudioCount = State#publisher.audio_count + (size(PCM) div 2),
+  AbsDelta = timer:now_diff(erlang:now(),State#publisher.start) div 1000,
+  StreamDelta = State#publisher.audio_count div (32*2),
+  % ?D({a, DTS, StreamDelta, AbsDelta, AbsDelta - StreamDelta}),
+  {noreply, State1#publisher{audio_count = AudioCount}};
 
 handle_info({rtmp, _, _} = Msg, State) ->
   io:format("rtmp: ~p~n", [Msg]),
